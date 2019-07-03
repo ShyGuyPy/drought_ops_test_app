8a8f7532b4f6e0115321036bb7f171d2e1ccd588
ShyGuyPy <luke.vawter1@gmail.com>
2019-07-03 15:12:46 -0400

    tabs

--- ui.R
+++ ui.R
@@ -1,5 +1,13 @@
 dashboardPage(
   dashboardHeader(title="drop_test"),
   dashboardSidebar(),
-  dashboardBody()
+  dashboardBody(
+    tabsetPanel(
+      tabPanel("Situational Awareness", plotOutput("plot")),
+      tabPanel("1-day ops", verbatimTextOutput("summary")),
+      tabPanel("10-day ops", tableOutput("table")),
+      tabPanel("Long-term ops", tableOutput("table")),
+      tabPanel("Download and Visualize", tableOutput("table"))
+    )
+  )
   )
