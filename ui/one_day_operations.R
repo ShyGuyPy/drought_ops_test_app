2632011e7ede53273fb6c28f3a2870f66b629ebe
ShyGuyPy <luke.vawter1@gmail.com>
2019-07-10 17:41:15 -0400

    maps are in placeholder positions.  need to be moved and (likely) resized tomorrow

--- ui/one_day_operations.R
+++ ui/one_day_operations.R
@@ -1,8 +1,16 @@
 tabPanel("One Day Operations",
          dashboardPage(
-           dashboardHeader(title = "one day"),
+           dashboardHeader(title = "one day"
+                           ),
            dashboardSidebar(),
            dashboardBody(
+             
+             tags$img(alt="Drought Status Map:2019-05-31",
+             src= "https://mde.maryland.gov/programs/Water/droughtinformation/Currentconditions/PublishingImages/DroughtGraphsStarting2019jan31/Drought2019-05-31.png",
+             style="width:200px;height:200px;border:0;")
+
+
+               
 
            ) #dashboardBody end
 
--- ui/ten_day_operations.R
+++ ui/ten_day_operations.R
@@ -3,6 +3,10 @@
       dashboardHeader(title = "10"),
       dashboardSidebar(),
       dashboardBody(
+        tags$img(alt="Drought Status Map:2019-05-31",
+                 src= "https://deq1.bse.vt.edu/drought/state/images/maps/imageMapFile15627929031512.png",  
+                 style="width:200px;height:200px;border:0;")
+        
 
       ) #dashboardBody end
 
